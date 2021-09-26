65cfa1485d99b55302d23ec9a92c43754ce46915
Claire Fahy <zeebear21@gmail.com>
2021-09-26 20:13:08 -0300

    up to 6a except 3d

--- proyecto2.hs
+++ proyecto2.hs
@@ -21,12 +21,15 @@ titulo MatematicaAplicada = "Licenciatura en Matematica Aplicada"
 type Ingreso = Int
 
 data Cargo = Titular | Asociado | Adjunto | Asistente | Auxiliar
+    deriving Show
 data Area = Administrativa | Ensenanza | Economica | Postgrado
+    deriving Show
 
 data Persona = Decane
             | Docente Cargo
             | NoDocente Area
             | Estudiante Carrera Ingreso
+    deriving Show
 
 -- ejercicio 3 (b)
 {-
@@ -35,7 +38,129 @@ Docente :: Cargo -> Persona
 -}
 
 -- ejercicio 3 (c)
-{-
+-- taken from class 06/09
+-- https://docs.google.com/document/d/1LG6gpk-2ouAMiKliZ1pWXX2_tup62Oa80Eqh2gGOF5k/edit
+
 cuantos_doc :: [Persona] -> Cargo -> Int
-cuantos_doc xs c = length (filter c xs)
+cuantos_doc [] _ = 0
+cuantos_doc ((Docente Titular): xs) Titular = 1 + cuantos_doc xs Titular
+cuantos_doc ((Docente Asociado): xs) Asociado = 1 + cuantos_doc xs Asociado
+cuantos_doc ((Docente Adjunto): xs) Adjunto = 1 + cuantos_doc xs Adjunto
+cuantos_doc ((Docente Asistente): xs) Asistente = 1 + cuantos_doc xs Asistente
+cuantos_doc ((Docente Auxiliar): xs) Auxiliar = 1 + cuantos_doc xs Auxiliar
+cuantos_doc (_: xs) c =  cuantos_doc xs c
+
+-- ejercicio 3 (d)
+
+esDocente :: Persona -> Bool
+esDocente (Docente _) = True
+esDocente _ = False
+-- or
+tieneCargo :: Persona -> Bool
+tieneCargo (Docente _) = True
+tieneCargo _ = False
+
+esDocCargo :: Persona -> Cargo -> Bool
+esDocCargo (Docente Titular) Titular = True
+esDocCargo (Docente Asociado) Asociado = True
+esDocCargo (Docente Adjunto) Adjunto = True
+esDocCargo (Docente Asistente) Asistente = True
+esDocCargo (Docente Auxiliar) Auxiliar = True
+esDocCargo _ _ = False
+
+{-
+<interactive>:124:1: error:
+    • Couldn't match expected type ‘Cargo -> t’
+                  with actual type ‘[Persona]’
+    • The function ‘filter’ is applied to three arguments,
+      but its type ‘(Persona -> Bool) -> [Persona] -> [Persona]’
+      has only two
+      In the expression: filter esDocCargo l Asociado
+      In an equation for ‘it’: it = filter esDocCargo l Asociado
+    • Relevant bindings include it :: t (bound at <interactive>:124:1)
+
+<interactive>:124:8: error:
+    • Couldn't match type ‘Cargo -> Bool’ with ‘Bool’
+      Expected type: Persona -> Bool
+        Actual type: Persona -> Cargo -> Bool
+    • Probable cause: ‘esDocCargo’ is applied to too few arguments
+      In the first argument of ‘filter’, namely ‘esDocCargo’
+      In the expression: filter esDocCargo l Asociado
+      In an equation for ‘it’: it = filter esDocCargo l Asociado
+
+In other words, I can only filter Persona, not Persona Cargo. "filter esDocente l" works fine
 -}
+
+{-
+let p = Docente Adjunto
+let d = Decane
+let t = Docente Titular
+let l = [(Docente Adjunto), (Docente Asociado), (Docente Asistente), (Decane), (NoDocente Postgrado), (Docente Asistente)]
+
+
+
+cuantos_doc' :: [Persona] -> Cargo -> Int
+cuantos_doc' p c = length (filter esDocCargo (filter esDocente p) c)
+
+-}
+
+----------------------------------------------------
+
+-- ejercicio 4 (a)
+
+primerElemento :: [a] -> Maybe a
+primerElemento [] = Nothing
+primerElemento l = Just (head l)
+
+----------------------------------------------------
+
+data Cola = VaciaC | Encolada Persona Cola
+    deriving Show
+
+mostrar :: Cola -> [Persona]
+mostrar VaciaC = []
+mostrar (Encolada p c) = p : mostrar c
+
+
+-- ejercicio 5 (a) (1)
+
+atender :: Cola -> Maybe Cola
+atender VaciaC = Nothing
+atender (Encolada _ c) = Just c
+
+{-
+let l = ['a', 'b', 'c', 'd', 'e']
+let c = Encolada (Docente Adjunto) (Encolada (Docente Asociado) (Encolada (Docente Asistente) (Encolada (Decane) (Encolada (NoDocente Postgrado) VaciaC))))
+
+No: let s = Encolada Docente (Encolada NoDocente (Encolada Docente (Encolada Decane (Encolada NoDocente VaciaC))))
+No: let t = Encolada Docente Adjunto (Encolada NoDocente Ensenanza (Encolada Docente Titular (Encolada Decane (Encolada NoDocente Postgrado VaciaC))))
+
+-}
+
+-- ejercicio 5 (a) (2)
+
+encolar :: Persona -> Cola -> Cola
+encolar p VaciaC = Encolada p VaciaC
+encolar p c = Encolada p c
+
+-- ejercicio 5 (a) (3)
+
+busca :: Cola -> Cargo -> Maybe Persona
+busca VaciaC _ = Nothing
+busca (Encolada p c) cargo | esDocCargo p cargo == True = Just p
+                           | otherwise = busca c cargo
+
+-- ejercicio 5 (b)
+-- Cola parece una lista
+
+----------------------------------------------------
+
+-- ejercicio 6 (a)
+
+data ListaAsoc a b = Vacia | Nodo a b ( ListaAsoc a b )
+
+
+type GuiaTel = ListaAsoc String Int
+
+
+-- ejercicio 6 (a)
