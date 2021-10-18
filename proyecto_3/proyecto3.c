b6e63f6dafe3af5d63f2576bebc25634ee5e1e4d
Claire Fahy <zeebear21@gmail.com>
2021-10-17 22:55:52 -0300

    clean up, create project folders

---
+++ proyecto_3/proyecto3.c
@@ -0,0 +1,52 @@
+/*
+----------------------------------------------------
+Proyecto 3 AyED1
+----------------------------------------------------
+*/
+
+// Ejercicio 1
+
+#include <stdio.h>
+#include <stdbool.h>
+int main(void)
+
+{
+    int x, y, z;
+    printf("Ingrese un valor para x\n");
+    scanf("%d", &x);
+    printf("Ingrese un valor para y\n");
+    scanf("%d", &y);
+    printf("Ingrese un valor para z\n");
+    scanf("%d", &z);
+
+    int i = x + y + 1;
+    int j = z * z + y * 45 - 15 * x;
+    int k = y - 2 == (x * 3 + 1) % 5;
+    int l = y / 2 * x;
+    int m = y < x * z;
+
+    printf("El resultado de x + y + 1 es %d\n", i);
+    printf("El resultado de z * z + y * 45 - 15 * x es %d\n", j);
+    printf("El resultado de y - 2 == (x * 3 + 1) mod 5 es %d\n", k);
+    printf("El resultado de y / 2 * x es %d\n", l);
+    printf("El resultado de y < x * z es %d\n", m);
+    return 0;
+}
+
+/*
+Ingrese un valor para x
+7
+Ingrese un valor para y
+3
+Ingrese un valor para z
+5
+El resultado de x + y + 1 es 11
+El resultado de z * z + y * 45 - 15 * x es 55
+El resultado de y - 2 == (x * 3 + 1) mod 5 es 0
+El resultado de y / 2 * x es 7
+El resultado de y < x * z es 1
+
+------
+
+
+*/
