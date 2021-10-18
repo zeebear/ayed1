204fd3b78736adec5082768053a32c087efe68d5
Claire Fahy <zeebear21@gmail.com>
2021-10-18 03:11:06 -0300

    ej5b complete

--- proyecto_3/proyecto3_ej5b.c
+++ proyecto_3/proyecto3_ej5b.c
@@ -12,49 +12,76 @@ int main(void)
 
 {
     int x, y, i, a, j;
-    printf("Ingrese un valor numérico para x (en el ejercicio original, es 4)\n");
+    printf("Ingrese un valor numérico para x (en el ejercicio original, es 13)\n");
+    scanf("%d", &x);
+    printf("Ingrese un valor numérico para y (en el ejercicio original, es 3)\n");
+    scanf("%d", &y);
+    printf("Ingrese un valor numérico para i (en el ejercicio original, es 0)\n");
     scanf("%d", &i);
-    printf("Ingrese un valor numérico para y (en el ejercicio original, es 400)\n");
+    printf("Ingrese un valor numérico para a (en el ejercicio original, es 5)\n");
+    scanf("%d", &a);
+    printf("Ingrese un valor numérico para j (en el ejercicio original, es 0)\n");
     scanf("%d", &j);
-    printf("Ingrese un valor numérico para x (en el ejercicio original, es 4)\n");
-    scanf("%d", &i);
-    printf("Ingrese un valor numérico para y (en el ejercicio original, es 400)\n");
-    scanf("%d", &j);
-    printf("Ingrese un valor numérico para y (en el ejercicio original, es 400)\n");
-    scanf("%d", &j);
+    int temp;
+    bool res; /* declaración de variables*/
+    printf("Ingrese un valor booleano para res (en el ejercicio original, es 0)\n");
+    scanf("%d", &temp);
+    res = temp;
 
     /*
-    i es la variable en programa 1.h
-    j es la variable en programa 1.i
+    x, y, i son las variables en programa 1
+    a, j, res son las variables en programa 2
     */
 
-    /* Programa 1.h */
-    while (i != 0)
+    /* Programa 1 */
+    i = 0;
+    while (x >= y)
     {
-        i = i - 1;
-        /* printf("i |-> %d\n", i); */
+        x = x - y;
+        i = i + 1;
+        /* printf("x |-> %d, y |-> %d, i |-> %d\n", x, y, i); */
     }
 
-    /* Programa 1.i */
-    while (j != 0)
+    /* Programa 2 */
+    j = 2;
+    res = true;
+    while (j < a && res == true)
     {
-        j = 0;
-        /* printf("j |-> %d\n", j); */
+        res = res && (a % j != 0);
+        j = j + 1;
+        /* printf("a |-> %d, j |-> %d, res |-> %d\n", a, j, res); */
     }
 
     return 0;
 }
 
 /*
-Usando printf(); para mostrar los valores de cada iteración:
+Programa 1:
+Luego de iteración 1:
+x |-> 10, y |-> 3, i |-> 1
+Luego de iteración 2:
+x |-> 7, y |-> 3, i |-> 2
+Luego de iteración 3:
+x |-> 4, y |-> 3, i |-> 3
+Luego de iteración 4:
+x |-> 1, y |-> 3, i |-> 4
 
-Ingrese un valor para x (en el ejercicio original, es 4)
-4
-Ingrese un valor para y (en el ejercicio original, es 400)
-400
-i |-> 3
-i |-> 2
-i |-> 1
-i |-> 0
-j |-> 0
+Primero, el programa pone el índice igual a 0. Después, cada iteración
+resta el valor de y del valor de x, y a la vez aumenta el índice con 1,
+hasta que el valor de x es menor que el valor de y.
+
+------
+
+Programa 2:
+Luego de iteración 1:
+a |-> 5, j |-> 3, res |-> 1
+Luego de iteración 2:
+a |-> 5, j |-> 4, res |-> 1
+Luego de iteración 3:
+a |-> 5, j |-> 5, res |-> 1
+
+Primero, el programa pone el índice igual a 2 (si el índice fuese 0 o 1,
+x % i != 0 sería falso para cualquier valor de x), y pone res igual a True.
+Después, cada iteración aumenta el índice con 1, hasta que x % i != 0 
+esté falso.
 */
