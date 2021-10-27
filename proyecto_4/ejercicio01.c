2d760b29f5b513298f6b4ee6670160b9d3216c2a
Claire Fahy <zeebear21@gmail.com>
2021-10-27 00:12:04 -0300

    start ejercicio01.c

---
+++ proyecto_4/ejercicio01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+
+void holaHasta(int n)
+{
+}
+
+int main(void)
+{
+    int x;
+    printf("Ingrese un valor numérico entero\n");
+    scanf("%d", &x);
+    assert(x > 0);
+}
