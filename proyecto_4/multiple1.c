d3718299c7a82f10e7a2c780a5bdd52285e4f90b
Claire Fahy <zeebear21@gmail.com>
2021-10-28 16:54:41 -0300

    ej 2a complete

---
+++ proyecto_4/multiple1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+
+/*
+var x, y : Int
+con X, Y : Int
+{Pre: x = X, y = Y}
+x, y := x + 1, x + y
+{Post: x = X + 1, y = X + Y}
+
+var x, y, z : Int
+con X, Y : Int
+{Pre: x = X, y = Y}
+z := x
+x := x + 1
+y := z + y
+{Post: x = X + 1, y = X + Y}
+
+*/
+
+int main(void)
+{
+    int x, y, z;
+    printf("Ingrese un valor numérico entero para X\n");
+    scanf("%d", &x);
+    printf("Ingrese un valor numérico entero para Y\n");
+    scanf("%d", &y);
+
+    printf("Pre: X = %d, Y = %d\n", x, y);
+    z = x;
+    x = x + 1;
+    y = z + y;
+    printf("Post: X = %d, Y = %d\n", x, y);
+
+    return 0;
+}
