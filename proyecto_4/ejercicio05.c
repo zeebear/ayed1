7ce30b4d47d4788ef74afb596749f8dc6a1a8279
Claire Fahy <zeebear21@gmail.com>
2021-10-29 12:42:36 -0300

    ej5 complete

--- proyecto_4/ejercicio05.c
+++ proyecto_4/ejercicio05.c
@@ -3,12 +3,42 @@
 
 void pedirArreglo(int a[], int n_max)
 {
+    int i = 0;
+    int n;
+
+    while (i < n_max)
+    {
+        printf("Ingrese el próximo número entero del arreglo:\n");
+        scanf("%d", &n);
+        a[i] = n;
+        i++;
+    }
 }
 
 void imprimeArreglo(int a[], int n_max)
 {
+    int i = 0;
+    int n;
+
+    printf("[");
+    while (i < n_max - 1)
+    {
+        n = a[i];
+        printf("%d, ", n);
+        i++;
+    }
+    n = a[i];
+    printf("%d]\n", n);
 }
 
 int main(void)
 {
+    int n_max;
+    printf("Ingrese la cantidad de números en el arreglo:\n");
+    scanf("%d", &n_max);
+
+    int a[n_max];
+    pedirArreglo(a, n_max);
+    imprimeArreglo(a, n_max);
+    return 0;
 }
