88756d42bf7c10966175e6ff0633139f1062fae1
Claire Fahy <zeebear21@gmail.com>
2021-10-30 19:15:56 -0300

    ej10 complete

--- proyecto_4/stats.c
+++ proyecto_4/stats.c
@@ -49,6 +49,27 @@ struct datos_t stats(float a[], int tam)
 
 int main(void)
 {
+    /* pide al usuario los datos del arreglo */
+    /* asumiendo un tamaño constante de 4 */
+    int tam = 4;
+    float a[4], n;
+
+    int i = 0;
+
+    while (i < tam)
+    {
+        printf("Ingrese el próximo número del arreglo:\n");
+        scanf("%f", &n);
+        a[i] = n;
+        i++;
+    }
+
+    struct datos_t resultados = stats(a, tam);
+    float max = resultados.maximo;
+    float min = resultados.minimo;
+    float prom = resultados.promedio;
+
+    printf("El máximo del arreglo es %f, el mínimo del arreglo es %f, y el promedio del arreglo es %f\n", max, min, prom);
 
     return 0;
 }
