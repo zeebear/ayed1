ffe5d6f03ea261bad752ceb9262336dd3734a024
Claire Fahy <zeebear21@gmail.com>
2021-11-01 16:05:09 -0300

    ej3 complete

--- parcialito_2/ejercicio3.c
+++ parcialito_2/ejercicio3.c
@@ -8,9 +8,58 @@ Parcialito 2 AyED1 2021
 
 #include <stdio.h>
 #include <assert.h>
+#include <stdbool.h>
+
+void pedirArreglo(int a[], int n_max)
+{
+    int i = 0;
+    int n;
+
+    while (i < n_max)
+    {
+        printf("Ingrese el próximo número entero del arreglo:\n");
+        scanf("%d", &n);
+        a[i] = n;
+        i++;
+    }
+}
+
+int mult_pares(int array[], int tam)
+{
+    int producto = 1;
+    int i = 0;
+    bool pares = false;
+
+    while (i < tam)
+    {
+        if (array[i] % 2 == 0)
+        {
+            producto = producto * array[i];
+        }
+        i++;
+    }
+
+    if (pares == false)
+    {
+        producto = 0;
+    }
+
+    return producto;
+}
 
 int main(void)
 {
+    /* armar el arreglo */
+    int N;
+    printf("Ingrese la cantidad de números en el arreglo:\n");
+    scanf("%d", &N);
+
+    int a[N];
+    pedirArreglo(a, N);
+
+    /* imprimir el resultado*/
+    int prod_pares = mult_pares(a, N);
+    printf("El producto de los números pares en el arreglo es %d\n", prod_pares);
 
     return 0;
 }
