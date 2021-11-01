f0a033539c08e61ee44aae540dcf1f42fc0f4c41
Claire Fahy <zeebear21@gmail.com>
2021-11-01 16:38:49 -0300

    ej4 just in time

--- parcialito_2/ejercicio4.c
+++ parcialito_2/ejercicio4.c
@@ -23,9 +23,12 @@ void pedirArreglo(struct asoc_t a[], int n_max)
 
     while (i < n_max)
     {
-        printf("Ingrese el próximo número entero del arreglo:\n");
+        printf("Ingrese el próximo clave del elemento del arreglo:\n");
         scanf("%d", &n);
-        a[i] = n;
+        a[i].clave = n;
+        printf("Ingrese el próximo valor del elemento del arreglo:\n");
+        scanf("%d", &n);
+        a[i].valor = n;
         i++;
     }
 }
@@ -34,12 +37,43 @@ bool hay_asoc(int key, struct asoc_t a[], int tam)
 {
     bool asoc = false;
 
+    int i = 0;
+    while (i < tam)
+    {
+        if (a[i].clave == key)
+        {
+            asoc = true;
+        }
+        i++;
+    }
+
     return asoc;
 }
 
 int main(void)
 {
-    struct asoc_t a[] = pedirArreglo;
+    int n_max;
+    printf("Ingrese la cantidad de elementos en el arreglo:\n");
+    scanf("%d", &n_max);
+
+    struct asoc_t a[n_max];
+    pedirArreglo(a, n_max);
+
+    int clave;
+    printf("Ingrese un clave para buscar en el arreglo de asociaciones:\n");
+    scanf("%d", &clave);
+
+    bool resultado = hay_asoc(clave, a, n_max);
+    char *resultado_s;
+    if (resultado == true)
+    {
+        resultado_s = "sí está";
+    }
+    else
+    {
+        resultado_s = "no está";
+    }
+    printf("El clave %d %s en el arreglo\n", clave, resultado_s);
 
     return 0;
 }
